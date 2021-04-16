24c4338fcadfe8639eb269c4f828f5d1a369887c
Alexey Pryadka <disableAPI@gmail.com>
2021-04-16 20:12:12 +0300

    Commit

---
+++ Program/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            List<double> inputTriangle = initTriangle();
+            double result = Сalculation.Сalculation.findSideOfTriangle(inputTriangle[0], inputTriangle[1]);
+
+            if (result.Equals(-1))
+            {
+                Console.WriteLine("Неверные данные поробуй еще!");
+                inputTriangle.Clear();
+                inputTriangle.AddRange(initTriangle());
+                result = Сalculation.Сalculation.findSideOfTriangle(inputTriangle[0], inputTriangle[1]);
+
+            }
+            Console.WriteLine($"Длина неизвестных сторон треугольника равна : {result}");
+
+            Console.WriteLine("\nЗадание 2");
+            List<double> inputSumOfmembersSeries = initfindSumOfmembersSeries();
+            result = Сalculation.Сalculation.findSumOfmembersSeries((int)inputSumOfmembersSeries[0], inputSumOfmembersSeries[1]);
+            
+            if (result.Equals(-1))
+            {
+                Console.WriteLine("Неверные данные поробуй еще!");
+                inputSumOfmembersSeries.Clear();
+                inputSumOfmembersSeries.AddRange(initfindSumOfmembersSeries());
+                result = Сalculation.Сalculation.findSideOfTriangle(inputTriangle[0], inputTriangle[1]);
+
+            }
+            Console.WriteLine($"S = {result}");
+
+            Console.WriteLine("\nЗадание 3");
+            List<double> inputExactSumOfmembersSeries = initfindSumOfmembersSeries();
+            result = Сalculation.Сalculation.findExactlySumOfmembersSeries((int)inputExactSumOfmembersSeries[0], inputExactSumOfmembersSeries[1]);
+
+            if (result.Equals(-1))
+            {
+                Console.WriteLine("Неверные данные поробуй еще!");
+                inputExactSumOfmembersSeries.Clear();
+                inputExactSumOfmembersSeries.AddRange(initTriangle());
+                result = Сalculation.Сalculation.findExactlySumOfmembersSeries((int)inputExactSumOfmembersSeries[0], inputExactSumOfmembersSeries[1]);
+
+            }
+            Console.WriteLine($"T = {result}");
+
+
+
+        }
+        // Secondary functions
+        static double aDouble(string numberToString) 
+        {
+            double number;
+            while (!double.TryParse(numberToString, out number))
+            {
+                Console.Write("Введите число!\n ->");
+                numberToString = Console.ReadLine();
+            }
+            return number;
+
+        }
+
+        static int aInt(string numberToString)
+        {
+            int number;
+            while (!int.TryParse(numberToString, out number))
+            {
+                Console.Write("Введите число!\n ->");
+                numberToString = Console.ReadLine();
+            }
+            return number;
+
+        }
+
+        static List<double> initTriangle()
+        {
+            Console.WriteLine("Значение периметра : ");
+            double P = aDouble(Console.ReadLine());
+            Console.WriteLine("Значение длины известной стороны : ");
+            double A = aDouble(Console.ReadLine());
+            return new List<double> { P, A };
+
+        }
+
+        static List<double> initfindSumOfmembersSeries()
+        {
+            Console.WriteLine("Количество итерацый : ");
+            int n = aInt(Console.ReadLine());
+            Console.WriteLine("Значение периметра x : ");
+            double x = aDouble(Console.ReadLine());
+            return new List<double> { n, x };
+
+        }
+    }
+}
---
+++ Сalculation/Сalculation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Сalculation
+{
+    public static class Сalculation
+    {
+       public static double findSideOfTriangle(double P , double A)
+       {
+            if (P > 0 && A > 0 && A < P)
+            {
+                return (P - A) / 2;
+            }
+            return -1;
+        }
+
+        public static double findSumOfmembersSeries(int n , double x)
+        {
+            if (n <= 0)
+            {
+                return -1;
+            }
+
+            double S = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                S += Math.Cos(x) + (Math.Pow(Math.Cos(x), i) / i);
+            }
+
+            return S;
+        }
+
+        public static double findExactlySumOfmembersSeries(int n, double x)
+        {
+            
+            if (n <= 0)
+            {
+                return -1;
+            }
+
+            double T = 0;
+            double sum;
+            double exact;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = 1 + 3 * Math.Pow(x, 2) + (5 * Math.Pow(x, 4) / Factorial(i)) + ((2*n + 1)/ Factorial(i)) * Math.Pow(x,2*i);
+                exact = (1 - 2 * Math.Pow(x, 2)) * Math.Exp(Math.Pow(x, 2));
+                T += (sum - exact) / exact * 1;
+
+            }
+
+            return T;
+        }
+
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
